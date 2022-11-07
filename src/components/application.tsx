8790c2c9027c853525710d6dfa1f4815bc6d53d1
Steve Kinney <hello@stevekinney.net>
2022-11-06 17:31:50 -0700

    Initialize project using Create React App

---
+++ src/components/application.tsx
@@ -0,0 +1,42 @@
+/**
+ * Things you could try:
+ *
+ * JSX.Element;
+ * JSX.Element | JSX.Element[];
+ * React.ReactNode;
+ * React.ReactChildren;
+ * React.ReactChild[];
+ */
+
+type BoxProps = { children: any /* 👈 Get rid of this! */ };
+
+const Box = ({ children }: BoxProps) => {
+  return (
+    <section
+      className="m-4"
+      style={{ padding: '1em', border: '5px solid purple' }}
+    >
+      {children}
+    </section>
+  );
+};
+
+const Application = () => {
+  return (
+    <main className="m-8">
+      <Box>
+        Just a string.
+        <p>Some HTML that is not nested.</p>
+        <Box>
+          <h2>Another React component with one child.</h2>
+        </Box>
+        <Box>
+          <h2 className="mb-4">A nested React component with two children.</h2>
+          <p>The second child.</p>
+        </Box>
+      </Box>
+    </main>
+  );
+};
+
+export default Application;
